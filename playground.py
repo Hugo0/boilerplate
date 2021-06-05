1705c4408730c9f3e98ca8e52e71c3c449ff9986
Hugo_Montenegro <hugo@festivalgallery.es>
2021-06-05 10:54:48 +0200

    added playground and removed .vscode from git history

---
+++ playground.py
@@ -0,0 +1,5 @@
+# Much of what starting a project looks like is trial-and-error. 
+# Testing out some API, or some functionality, or whatever.
+# The playground is intended as a temporary file where you can conduct such experiments
+# An alternative is to use a Jupyter notebook (playground.ipynb), which is
+# ideal for interactive experimentation.
